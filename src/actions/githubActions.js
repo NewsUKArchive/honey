e45c3114a3fc3faf4af8d65bb3d92fa91d9356ca
Sam Hendriksen <sam.hendriksen@ecs.co.uk>
2018-03-27 10:33:27 +0100

    chore: removed unneeded data files.

--- src/actions/githubActions.js
+++ src/actions/githubActions.js
@@ -1,7 +1,5 @@
 import repositories from '../config/repositories';
 import requestBuilder from '../helpers/requestBuilder';
-import timesComponentIssues from './../data/mockgithubissuesTimesComponent.json';
-import dextroseIssues from './../data/mockgithubissuesDextrose.json';
 
 export const GITHUB_FETCH_ISSUES = 'GITHUB_ISSUES_FETCH';
 
