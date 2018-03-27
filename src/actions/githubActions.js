eae3f592fa25db2c4088f82f169d2e2680ba657f
Sam Hendriksen <sam.hendriksen@ecs.co.uk>
2018-03-27 10:01:04 +0100

    chore: cleaned up the fetch issues action.

--- src/actions/githubActions.js
+++ src/actions/githubActions.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
 import repositories from '../config/repositories';
+import requestBuilder from '../helpers/requestBuilder';
 import timesComponentIssues from './../data/mockgithubissuesTimesComponent.json';
 import dextroseIssues from './../data/mockgithubissuesDextrose.json';
 
@@ -8,44 +8,17 @@ export const GITHUB_FETCH_ISSUES = 'GITHUB_ISSUES_FETCH';
 export function fetchIssues() {
     return (dispatch) => {
 
-        const fetchRequests = [];
-
-        Object
-            .keys(repositories)
-            .forEach(key => {
-                const value = repositories[key];
-                console.log(`Key: ${key} Value: ${value}`);
-
-                fetchRequests.push(new Promise(resolve => {
-                    axios
-                        .get(`https://api.github.com/repos/${value}/issues?state=all`)
-                        .then((result) => {
-                            resolve({project: key, issues: result.data});
-                        })
-                        .catch((error) => {
-                            resolve({project: key, issues: []});
-                        })
-                }));
-
-            });
-
         Promise
-            .all(fetchRequests)
+            .all(requestBuilder.buildIssueRequests(repositories))
             .then((results) => {
                 return results.reduce((object, currentValue) => {
-                    object = Object.assign(object, {
+                    return Object.assign(object, {
                         [currentValue.project]: currentValue.issues
                     });
-
-                    return object;
                 }, {});
             })
-            .then(payload => {
-                console.log('payload:', payload)
-                dispatch({type: GITHUB_FETCH_ISSUES, payload});
-            })
-            .catch((error) => {
-                dispatch({type: GITHUB_FETCH_ISSUES, payload: error, error: true});
-            })
+            .then(payload => dispatch({type: GITHUB_FETCH_ISSUES, payload}))
+            .catch((error) => dispatch({type: GITHUB_FETCH_ISSUES, payload: error, error: true}));
     };
+
 }
