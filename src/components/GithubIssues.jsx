fcfca61b43754367ff692140c4fdf6551445a2f0
Sam Hendriksen <sam.hendriksen@ecs.co.uk>
2018-04-17 13:36:39 +0100

    Fix/authentication (#13)
    * Refactored to use the friendly names.
    
    * Added private repos. Removed total count as it was skewing the results.
    
    * github.js refactored.

--- src/components/GithubIssues.jsx
+++ src/components/GithubIssues.jsx
@@ -1,67 +1,47 @@
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import React from 'react';
-import { bindActionCreators } from 'redux';
-import * as githubActions from '../actions/githubActions';
+import {bindActionCreators} from 'redux';
+import {fetchOpenIssues} from '../actions/githubActions';
 import LoadingComponent from './LoadingComponent';
 import RadarComponent from 'react-d3-radar';
 
-let data = {
-  variables: [],
-  sets: [
-    {
-      key: 'Total Issues',
-      label: 'Total Issues',
-      values: {},
-    },
-    {
-      key: 'Open Issues',
-      label: 'Open Issues',
-      values: {},
-    },
-  ],
-};
-
-
 const renderData = projects => {
-  projects.totalIssues.map(project => {
-    data.variables.push({key: project.name, label: project.name});
-    data.sets[0].values[project.name] = project.issues.totalCount;
-    return data}
-  );
-  projects.openIssues.map(project => {
-    data.sets[1].values[project.name] = project.issues.totalCount;
-    return data}
-  );
-  return data;   
-}
+  const variables = projects.openIssues.map(({name}) => ({key: name, label: name}));
+  const values = projects.openIssues.reduce((result, {name, issues}) => ({
+    ...result, 
+    [name]: issues.totalCount
+  }), {});
+
+  return {
+    variables,
+    sets: [
+      {
+        key: 'Open Issues',
+        label: 'Open Issues',
+        values
+      }
+    ]
+  };
+};
 
 class GithubIssues extends React.Component {
   componentWillMount() {
-    this.props.github.fetchTotalIssues();
-    this.props.github.fetchOpenIssues();    
+    this.props.fetchOpenIssues();
   }
 
   render() {
-    if (!this.props.projects.openIssues || !this.props.projects.totalIssues) return <LoadingComponent/>;
+    if (!this.props.projects.openIssues) return <LoadingComponent/>;
     
-    var radar = <div style={myStyle}>
+    return (
+      <div style={myStyle}>
         <RadarComponent
           width={600}
           height={600}
           padding={70}
-          domainMax={
-            Math.max.apply(
-              Math,this.props.projects.totalIssues.map((project) => {
-                return project.issues.totalCount;
-              })
-            )
-          }
+          domainMax={Math.max(...this.props.projects.openIssues.map((project) => project.issues.totalCount))}
           highlighted={null}
-          data={renderData(this.props.projects)}     
-        />
+          data={renderData(this.props.projects)}/>
       </div>
-    return (
-      radar
     );
   }
 }
@@ -73,18 +53,13 @@ const myStyle = {
 };
 
 function mapStateToProps(state) {
-  return {
-    projects: state.github,
-  };
+  return {projects: state.github};
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    github: bindActionCreators(githubActions, dispatch),
+    fetchOpenIssues: bindActionCreators(fetchOpenIssues, dispatch)
   };
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(GithubIssues);
+export default connect(mapStateToProps, mapDispatchToProps,)(GithubIssues);
--- src/provider/github.js
+++ src/provider/github.js
@@ -1,65 +1,78 @@
-import { createApolloFetch } from 'apollo-fetch';
+import {createApolloFetch} from 'apollo-fetch';
 
 const uri = 'https://api.github.com/graphql';
 
-const apolloFetch = createApolloFetch({ uri });
+const apolloFetch = createApolloFetch({uri});
 
-apolloFetch.use(({ options }, next) => {
+const parseIssues = (project) => (project && project.data && project.data.repository && project.data.repository.issues)
+  ? project.data.repository.issues : {totalCount: 0};
+
+apolloFetch.use(({
+  options
+}, next) => {
   if (!options.headers) {
     // eslint-disable-next-line no-param-reassign
     options.headers = {
-      authorization: `bearer ${process.env.REACT_APP_GITHUB_KEY}`,
+      authorization: `bearer ${process.env.REACT_APP_GITHUB_KEY}`
     };
   }
   next();
 });
 
-const totalIssuesGetRequest = project =>
-  new Promise((resolve) => {
-    apolloFetch({
-      query: `query IssueCount($owner: String!, $repositoryName: String!) {
-        repository(owner: $owner, name:$repositoryName) {
+const totalIssuesGetRequest = project => apolloFetch({
+  query: `query IssueCount($owner: String!, $repositoryName: String!) {
+        repository(owner: $owner, name: $repositoryName) {
           issues() {
             totalCount
           }
         }
       }`,
-      variables: {
-        repositoryName: project.repository,
-        owner: project.owner,
-      },
-    })
-      .then((response) => {
-        resolve({ name: project.repository, issues: response.data.repository.issues });
-      })
-      .catch(err => resolve(err));
-  });
+  variables: {
+    repositoryName: project.repository,
+    owner: project.owner
+  }
+}).then((response) => {
+  if (response.errors) 
+    throw response;
+  return response;
+}).catch(badResponse => {
+  console.error(`Failed to get the open issues for ${project.name}. Error: `, badResponse.errors);
+  return badResponse;
+}).then(response => ({
+  name: project.name,
+  repository: project.repository,
+  issues: parseIssues(response),
+}));
 
-const openIssuesGetRequest = project =>
-  new Promise((resolve) => {
-    apolloFetch({
-      query: `query IssueCount($owner: String!, $repositoryName: String!) {
-        repository(owner: $owner, name:$repositoryName) {
-          issues(states:OPEN) {
+const openIssuesGetRequest = project => apolloFetch({
+  query: `query IssueCount($owner: String!, $repositoryName: String!) {
+        repository(owner: $owner, name: $repositoryName) {
+          issues(states: OPEN) {
             totalCount
           }
         }
       }`,
-      variables: {
-        repositoryName: project.repository,
-        owner: project.owner,
-      },
-    })
-      .then((response) => {
-        resolve({ name: project.repository, issues: response.data.repository.issues });
-      })
-      .catch(err => resolve(err));
-  });
+  variables: {
+    repositoryName: project.repository,
+    owner: project.owner
+  }
+}).then((response) => {
+  if (response.errors) 
+    throw response;
+  return response;
+}).catch(badResponse => {
+  console.error(`Failed to get the open issues for ${project.name}. Error: `, badResponse.errors);
+  return badResponse;
+}).then(response => ({
+  name: project.name,
+  repository: project.repository,
+  issues: parseIssues(response),
+}));
 
-const getTotalIssueCountFor = projects =>
-  Object.values(projects).map(project => (totalIssuesGetRequest(project)));
+const getTotalIssueCountFor = projects => projects.map(project => totalIssuesGetRequest(project));
+const getOpenIssueCountFor = projects => projects.map(project => openIssuesGetRequest(project));
 
-const getOpenIssueCountFor = projects =>
-  Object.values(projects).map(project => (openIssuesGetRequest(project)));
-
-export default { getTotalIssueCountFor, getOpenIssueCountFor };
+export default {
+  getTotalIssueCountFor,
+  getOpenIssueCountFor
+};
